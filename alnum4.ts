bb05d6f51c081ffa9b0ea76932f3ae1fbf891045
Sam Gao <gaoshan_274@hotmail.com>
2019-03-21 15:59:59 +0000

    testing fixes

--- alnum4.ts
+++ alnum4.ts
@@ -1,9 +1,9 @@
+//% color=#008080 weight=100 icon="\uf26c"
 declare namespace HT16K33_Alnum4 {
 
     /**
      * Prints a text on the alnum display, will scroll with interval if more than 4 letters 
      */
-    //% help=basic/show-string 
     //% weight=87 blockGap=8
     //% block="show|string %text" 
     //% async
@@ -11,19 +11,20 @@ declare namespace HT16K33_Alnum4 {
     //% icon="\uf1ec" interval.defl=250 shim=HT16K33_Alnum4::showString
     export function showString(text: string, interval?: number): void {
         console.log("alphanumeric display:" + text);
+        return;
     }
 
     /**
      * Scroll a number on the screen. If the number fits on the screen (i.e. less than 4 digit), do not scroll.
      * Defaults to flush right
      */
-    //% help=basic/show-number
     //% weight=96
     //% blockId=alnum_print_number 
     //% block="show|number %number" blockGap=8
     //% async rightAlign.defl=1 interval.defl=250 shim=HT16K33_Alnum4::showNumber
     export function showNumber(value: number, rightAlign?: boolean, interval?: number): void {
         console.log("alphanumeric display:" + value);
+        return;
     }
 
     /**
@@ -32,5 +33,7 @@ declare namespace HT16K33_Alnum4 {
     //% blockId=alnum_init
     //% block="Initialize Alphanumeric Display"
     //% icon="\uf1ec" shim=HT16K33_Alnum4::init
-    export function init(): void {}
+    export function init(): void {
+        return;
+    }
 }
