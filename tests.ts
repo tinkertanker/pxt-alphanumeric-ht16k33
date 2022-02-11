639f3fcbbc2c6e78e1cb756609536d20718bc69c
Mireille T <69097387+Mireille-T@users.noreply.github.com>
2022-02-11 11:51:55 +0800

    Add test case

--- tests.ts
+++ tests.ts
@@ -1,1 +1,6 @@
 // tests go here; this will not be compiled when this package is used as a library
+HT16K33_Alnum4.init();
+serial.writeString("Hello\r\n");
+basic.forever(() => {
+    HT16K33_Alnum4.showNumber(1234);
+})
