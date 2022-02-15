e6863b08f87751fcb6c8ff6634a836097d37d45b
Mireille T <69097387+Mireille-T@users.noreply.github.com>
2022-02-15 16:15:03 +0800

    Update pxt.json, alnum4.ts, tests.ts

--- alnum4.ts
+++ alnum4.ts
@@ -1,221 +1,183 @@
 //% color=#008080 weight=100 icon="\uf26c"
 namespace HT16K33_Alnum4 {
-    let HT16K33_ADDRESS = 0xE0;
-    let HT16K33_ON = 0x21;  // Commands
-    let HT16K33_STANDBY = 0x20;
-    let HT16K33_DISPLAYON = 0x81;
-    let HT16K33_DISPLAYOFF = 0x80;
-    let HT16K33_BRIGHTNESS = 0xE0;  // 0xE0|F from 0..15: Set brightness from 1/16 (0x00) to 16/16 (0x0F)
+    let HT16K33_ADDR = 0x70;
+    let HT16K33_BLINK_CMD = 0x80; // I2C register for BLINK setting
+    let HT16K33_BLINK_DISPLAYON = 0x01; // I2C value for steady on
+    let HT16K33_BLINK_OFF = 0; // I2C value for steady off
+    let HT16K33_BLINK_2HZ = 1; // I2C value for 2 Hz blink
+    let HT16K33_BLINK_1HZ = 2; // I2C value for 1 Hz blink
+    let HT16K33_BLINK_HALFHZ = 3; // I2C value for 0.5 Hz blink
+    let HT16K33_CMD_BRIGHTNESS = 0xE0; // I2C register for BRIGHTNESS setting
 
-    const alphafonttable = [
-        0b0000000000000001,
-        0b0000000000000010,
-        0b0000000000000100,
-        0b0000000000001000,
-        0b0000000000010000,
-        0b0000000000100000,
-        0b0000000001000000,
-        0b0000000010000000,
-        0b0000000100000000,
-        0b0000001000000000,
-        0b0000010000000000,
-        0b0000100000000000,
-        0b0001000000000000,
-        0b0010000000000000,
-        0b0100000000000000,
-        0b1000000000000000,
-        0b0000000000000000,
-        0b0000000000000000,
-        0b0000000000000000,
-        0b0000000000000000,
-        0b0000000000000000,
-        0b0000000000000000,
-        0b0000000000000000,
-        0b0000000000000000,
-        0b0001001011001001,
-        0b0001010111000000,
-        0b0001001011111001,
-        0b0000000011100011,
-        0b0000010100110000,
-        0b0001001011001000,
-        0b0011101000000000,
-        0b0001011100000000,
-        0b0000000000000000, //  
-        0b0000000000000110, // !
-        0b0000001000100000, // "
-        0b0001001011001110, // #
-        0b0001001011101101, // $
-        0b0000110000100100, // %
-        0b0010001101011101, // &
-        0b0000010000000000, // '
-        0b0010010000000000, // (
-        0b0000100100000000, // )
-        0b0011111111000000, // *
-        0b0001001011000000, // +
-        0b0000100000000000, // ,
-        0b0000000011000000, // -
-        0b0100000000000000, // .
-        0b0000110000000000, // /
-        0b0000000000111111, // 0
-        0b0000000000000110, // 1
-        0b0000000011011011, // 2
-        0b0000000011001111, // 3
-        0b0000000011100110, // 4
-        0b0000000001101101, // 5
-        0b0000000011111101, // 6
-        0b0000000000000111, // 7
-        0b0000000011111111, // 8
-        0b0000000011101111, // 9
-        0b0001001000000000, // :
-        0b0000101000000000, // ;
-        0b0010010000000000, // <
-        0b0000000011001000, // =
-        0b0000100100000000, // >
-        0b0001000010000011, // ?
-        0b0000001010111011, // @
-        0b0000000011110111, // A
-        0b0001001010001111, // B
-        0b0000000000111001, // C
-        0b0001001000001111, // D
-        0b0000000011111001, // E
-        0b0000000001110001, // F
-        0b0000000010111101, // G
-        0b0000000011110110, // H
-        0b0001001000000000, // I
-        0b0000000000011110, // J
-        0b0010010001110000, // K
-        0b0000000000111000, // L
-        0b0000010100110110, // M
-        0b0010000100110110, // N
-        0b0000000000111111, // O
-        0b0000000011110011, // P
-        0b0010000000111111, // Q
-        0b0010000011110011, // R
-        0b0000000011101101, // S
-        0b0001001000000001, // T
-        0b0000000000111110, // U
-        0b0000110000110000, // V
-        0b0010100000110110, // W
-        0b0010110100000000, // X
-        0b0001010100000000, // Y
-        0b0000110000001001, // Z
-        0b0000000000111001, // [
-        0b0010000100000000, // 
-        0b0000000000001111, // ]
-        0b0000110000000011, // ^
-        0b0000000000001000, // _
-        0b0000000100000000, // `
-        0b0001000001011000, // a
-        0b0010000001111000, // b
-        0b0000000011011000, // c
-        0b0000100010001110, // d
-        0b0000100001011000, // e
-        0b0000000001110001, // f
-        0b0000010010001110, // g
-        0b0001000001110000, // h
-        0b0001000000000000, // i
-        0b0000000000001110, // j
-        0b0011011000000000, // k
-        0b0000000000110000, // l
-        0b0001000011010100, // m
-        0b0001000001010000, // n
-        0b0000000011011100, // o
-        0b0000000101110000, // p
-        0b0000010010000110, // q
-        0b0000000001010000, // r
-        0b0010000010001000, // s
-        0b0000000001111000, // t
-        0b0000000000011100, // u
-        0b0010000000000100, // v
-        0b0010100000010100, // w
-        0b0010100011000000, // x
-        0b0010000000001100, // y
-        0b0000100001001000, // z
-        0b0000100101001001, // {
-        0b0001001000000000, // |
-        0b0010010010001001, // }
-        0b0000010100100000, // ~
-        0b0011111111111111,
-    ];
+    let displaybuffer: Array<NumberFormat.UInt16BE> = [0, 0, 0, 0, 0, 0, 0, 0];
 
-    let displaybuffer = pins.createBuffer(8);
-
-    function command(c: number) {
-        let cmd = c;
-        pins.i2cWriteNumber(HT16K33_ADDRESS, cmd, NumberFormat.Int8LE);
+    interface Map {
+        [key: string]: number;
     }
 
-    function setBrightness(b: number) {
-        /* Brightness can lety from 0 to 15 */
-        if (b > 15) b = 15;
-        command(HT16K33_BRIGHTNESS | b);
+    const charBits: Map = {
+        " ": 0b0000000000000000,
+        "!": 0b0000000000000110,
+        "\"": 0b0000001000100000,
+        "#": 0b0001001011001110,
+        "$": 0b0001001011101101,
+        "%": 0b0000110000100100,
+        "&": 0b0010001101011101,
+        "\'": 0b0000010000000000,
+        "(": 0b0010010000000000,
+        ")": 0b0000100100000000,
+        "*": 0b0011111111000000,
+        "+": 0b0001001011000000,
+        ",": 0b0000100000000000,
+        "-": 0b0000000011000000,
+        ".": 0b0100000000000000,
+        "/": 0b0000110000000000,
+        "0": 0b0000110000111111,
+        "1": 0b0000000000000110,
+        "2": 0b0000000011011011,
+        "3": 0b0000000010001111,
+        "4": 0b0000000011100110,
+        "5": 0b0010000001101001,
+        "6": 0b0000000011111101,
+        "7": 0b0000000000000111,
+        "8": 0b0000000011111111,
+        "9": 0b0000000011101111,
+        ":": 0b0001001000000000,
+        ";": 0b0000101000000000,
+        "<": 0b0010010000000000,
+        "=": 0b0000000011001000,
+        ">": 0b0000100100000000,
+        "?": 0b0001000010000011,
+        "@": 0b0000001010111011,
+        "A": 0b0000000011110111,
+        "B": 0b0001001010001111,
+        "C": 0b0000000000111001,
+        "D": 0b0001001000001111,
+        "E": 0b0000000011111001,
+        "F": 0b0000000001110001,
+        "G": 0b0000000010111101,
+        "H": 0b0000000011110110,
+        "I": 0b0001001000001001,
+        "J": 0b0000000000011110,
+        "K": 0b0010010001110000,
+        "L": 0b0000000000111000,
+        "M": 0b0000010100110110,
+        "N": 0b0010000100110110,
+        "O": 0b0000000000111111,
+        "P": 0b0000000011110011,
+        "Q": 0b0010000000111111,
+        "R": 0b0010000011110011,
+        "S": 0b0000000011101101,
+        "T": 0b0001001000000001,
+        "U": 0b0000000000111110,
+        "V": 0b0000110000110000,
+        "W": 0b0010100000110110,
+        "X": 0b0010110100000000,
+        "Y": 0b0001010100000000,
+        "Z": 0b0000110000001001,
+        "[": 0b0000000000111001,
+        "\\": 0b0010000100000000,
+        "]": 0b0000000000001111,
+        "^": 0b0000110000000011,
+        "_": 0b0000000000001000,
+        "`": 0b0000000100000000,
+        "a": 0b0001000001011000,
+        "b": 0b0010000001111000,
+        "c": 0b0000000011011000,
+        "d": 0b0000100010001110,
+        "e": 0b0000100001011000,
+        "f": 0b0000000001110001,
+        "g": 0b0000010010001110,
+        "h": 0b0001000001110000,
+        "i": 0b0001000000000000,
+        "j": 0b0000000000001110,
+        "k": 0b0011011000000000,
+        "l": 0b0000000000110000,
+        "m": 0b0001000011010100,
+        "n": 0b0001000001010000,
+        "o": 0b0000000011011100,
+        "p": 0b0000000101110000,
+        "q": 0b0000010010000110,
+        "r": 0b0000000001010000,
+        "s": 0b0010000010001000,
+        "t": 0b0000000001111000,
+        "u": 0b0000000000011100,
+        "v": 0b0010000000000100,
+        "w": 0b0010100000010100,
+        "x": 0b0010100011000000,
+        "y": 0b0010000000001100,
+        "z": 0b0000100001001000,
+        "{": 0b0000100101001001,
+        "|": 0b0001001000000000,
+        "}": 0b0010010010001001,
+        "~": 0b0000010100100000,
     }
 
-    function blinkRate(b: number) {
+    function setBrightness(b: NumberFormat.UInt8BE) {
+        if (b > 15) b = 15; // limit to max brightness
+        let buffer = HT16K33_CMD_BRIGHTNESS | b;
+        pins.i2cWriteNumber(HT16K33_ADDR, buffer, NumberFormat.UInt8BE)
+    }
+
+    function blinkRate(b: NumberFormat.UInt8BE) {
         if (b > 3) b = 0; // turn off if not sure
-        /*  0: Blinking off
-          1: Blink at 2Hz
-          2: Blink at 1Hz
-          3: Blink at 0.5Hz
-        */
-        command(HT16K33_DISPLAYON | (b << 1));
+        let buffer = HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (b << 1);
+        pins.i2cWriteNumber(HT16K33_ADDR, buffer, NumberFormat.UInt8BE);
     }
 
     function begin() {
-        command(HT16K33_ON);
-        command(HT16K33_DISPLAYON);
-        setBrightness(15);
+        // turn on oscillator
+        pins.i2cWriteNumber(HT16K33_ADDR, 0x21, NumberFormat.UInt8BE);
+
+        // internal RAM powers up with garbage/random values.
+        // ensure internal RAM is cleared before turning on display
+        // this ensures that no garbage pixels show up on the display
+        // when it is turned on.
+        clear();
+        writeDisplay();
+        blinkRate(HT16K33_BLINK_OFF);
+        setBrightness(15); // max brightness
     }
 
     function writeDisplay() {
-        let buff = pins.createBuffer(9);
-        buff.setNumber(NumberFormat.Int8LE, 0, 0x00);
-        /* Changes the mapping of characters
-        0 -> 2
-        1 -> 3
-        2 -> 0
-        3 -> 1
-        */
-        for (let i = 0, p; i < 4; i++) {
-            p = i ^ 2;  //new mapped position
-            buff.setNumber(NumberFormat.Int8LE, (p << 1) | 1, displaybuffer.getNumber(NumberFormat.Int8LE, i) & 0xFF);
-            buff.setNumber(NumberFormat.Int8LE, (p << 1) + 2, displaybuffer.getNumber(NumberFormat.Int8LE, i) >> 8);
+        let buffer = pins.createBuffer(9);
+        buffer[0] = 0x00; // start at address 00
+        for (let i = 0; i < 4; i++) {
+            let p = i ^ 2;  //new mapped position
+            buffer[(p << 1) | 1] = displaybuffer[i] & 0xFF;
+            buffer[(p << 1) + 2] = displaybuffer[i] >> 8;
         }
-        serial.writeString("buff: ");
-        serial.writeBuffer(buff);
-        serial.writeLine("");
-        pins.i2cWriteBuffer(HT16K33_ADDRESS, buff, false);
+        pins.i2cWriteBuffer(HT16K33_ADDR, buffer);
     }
 
-    function clearBuffer() {
-        displaybuffer.fill(0);
+    function clear() {
+        for (let i = 0; i < 8; i++) {
+            displaybuffer[i] = 0;
+        }
     }
 
-    function writeRaw(position: number, bitmask: number) {
-        displaybuffer.setNumber(NumberFormat.Int8LE, position, bitmask);
+    function writeDigitRaw(n: NumberFormat.UInt8BE, bitmask: NumberFormat.UInt16BE) {
+        displaybuffer[n] = bitmask;
     }
 
-    function writeAscii(position: number, a: string, dot = false) {
-        let bitmask = alphafonttable[parseInt(a)];
-        serial.writeValue("bitmask", bitmask);
-        /* On HT16K33, position of bit 11 and 13 are swapped compared to AdaFruit */
+    function writeAscii(n: number, a: string, dot = false) {
+        let bitmask: NumberFormat.UInt16BE = charBits[a];
+        if (bitmask = undefined) bitmask = 0b0000000000000000;
         if (!!(bitmask & (1 << 11)) != !!(bitmask & (1 << 13))) {
             bitmask ^= (1 << 11) ^ (1 << 13);
         }
-        //dot is the 15th bit
-        if (dot) bitmask |= (1 << 14);
-
-        displaybuffer.setNumber(NumberFormat.Int8LE, position, bitmask);
+        if (dot) bitmask |= (1 << 14); //dot is the 15th bit
+        displaybuffer[n] = bitmask;
     }
 
     function scroll(s: string, interval = 250) {
         let L = s.length;
         if (interval < 0 || L <= 0) {
-            clearBuffer();
+            clear();
             writeDisplay();
             return;
         }
-        let seq: Array<uint16> = [];
+        let seq: Array<NumberFormat.UInt16BE> = [];
         for (let i = 0; i < L; i++) {
             if (i && s.charAt(i) == '.') {
                 if ((seq[seq.length - 1] & (1 << 14)) == 0) {
@@ -223,28 +185,25 @@ namespace HT16K33_Alnum4 {
                     continue;
                 }
             }
-            let bitmask = alphafonttable[parseInt(s.charAt(i))];
-            //let bitmask = 0b0000100001001000; // z
+            //let bitmask = alphafonttable[parseInt(s.charAt(i))];
+            let bitmask = charBits[s[i]];
             if (!!(bitmask & (1 << 11)) != !!(bitmask & (1 << 13)))
                 bitmask ^= (1 << 11) ^ (1 << 13);
             seq.push(bitmask);
         }
         L = seq.length;
         if (L <= 4) {
-            clearBuffer();
+            clear();
             for (let i = 0; i < L; i++) {
                 let p = 4 - L + i;
-                writeRaw(p, seq[i]);
+                writeDigitRaw(p, seq[i]);
             }
-            serial.writeString("displaybuffer: ");
-            serial.writeBuffer(displaybuffer);
-            serial.writeLine("");
             writeDisplay();
         } else {
             for (let i = -3; i <= L; i++) {
                 for (let p = 0; p < 4; p++) {
-                    if (i + p < 0 || i + p >= L) writeRaw(p, 0);
-                    else writeRaw(p, seq[i + p]);
+                    if (i + p < 0 || i + p >= L) writeDigitRaw(p, 0);
+                    else writeDigitRaw(p, seq[i + p]);
                 }
                 writeDisplay();
                 basic.pause(interval);
@@ -275,7 +234,7 @@ namespace HT16K33_Alnum4 {
     //% async rightAlign.defl=1 interval.defl=250
     export function showNumber(value: number, rightAlign?: boolean, interval?: number): void {
         if (interval < 0) {
-            clearBuffer();
+            clear();
             writeDisplay();
             return;
         }
@@ -287,7 +246,7 @@ namespace HT16K33_Alnum4 {
                 return;
             }
             /* assume its 3 digits */
-            clearBuffer();
+            clear();
             writeAscii(0, '-');
             for (let i = 0; i < s.length; i++) {
                 let p = 3 - s.length + i + 1;
@@ -301,7 +260,7 @@ namespace HT16K33_Alnum4 {
                 return;
             }
             /* assume its 4 digits */
-            clearBuffer();
+            clear();
             for (let i = 0; i < s.length; ++i) {
                 let p = 4 - s.length + i;
                 writeAscii(p, s.charAt(i));
@@ -317,8 +276,6 @@ namespace HT16K33_Alnum4 {
     //% block="Initialize Alphanumeric Display"
     //% icon="\uf1ec"
     export function init(): void {
-        displaybuffer.fill(0);
         begin();
-        writeDisplay();
     }
 }
--- tests.ts
+++ tests.ts
@@ -1,6 +1,8 @@
 // tests go here; this will not be compiled when this package is used as a library
+serial.setBaudRate(9600);
 HT16K33_Alnum4.init();
-serial.writeString("Hello\r\n");
 basic.forever(() => {
-    HT16K33_Alnum4.showNumber(1234);
+    HT16K33_Alnum4.showString("Hello, World!");
+    HT16K33_Alnum4.showNumber(-1.23456);
+    basic.pause(5000);
 })
